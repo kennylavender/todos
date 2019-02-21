2657ca42d2186bc75432516f36237d097cca17a0
Kenny Lavender <5175120+kennylavender@users.noreply.github.com>
2019-02-21 12:13:33 -0800

    split into mono repo

---
+++ api/source/server/app.js
@@ -0,0 +1,22 @@
+const graphqlHTTP = require('express-graphql');
+const express = require('express');
+const schema = require('./schema')
+
+const dev = process.env.NODE_ENV !== 'production'
+
+const createApp = () => new Promise((resolve, reject) => {
+  const app = express()
+
+  app.use(
+    '/graphql',
+    graphqlHTTP({
+      schema,
+      graphiql: true,
+      context: {},
+    })
+  )
+
+  return resolve(app)
+})
+
+module.exports = createApp
---
+++ api/source/server/index.js
@@ -0,0 +1,9 @@
+const app = require('./app')
+
+app().then(app => {
+  app.listen(3000, err => {
+    if (err) throw err
+    console.log('> Ready on http://localhost:3000')
+  })
+})
+
